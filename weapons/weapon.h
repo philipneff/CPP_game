121a317f3db2927f4a9d6540cafe881a428711ab
DavinSalo <davinsalo@cmail.carleton.ca>
2019-03-19 21:17:13 -0400

    Fixing README.md file's code block examples

---
+++ weapons/weapon.h
@@ -0,0 +1,42 @@
+#ifndef WEAPON_H
+#define WEAPON_H
+//this is going to be an interface
+#include <iostream>
+#include <string>
+
+
+
+class Weapon
+{
+    public:
+      
+        virtual int getDamageMin()=0;
+        virtual void setDamageMin(int)=0;
+
+        virtual int getDamageMax()=0;
+        virtual void setDamageMax(int)=0;
+
+        virtual int getAttributeDamageBonus()=0;
+        virtual void setAttributeDamageBonus(int)=0;
+
+        virtual void setAttributeToHitBonus(int)=0;
+        virtual int getAttributeToHitBonus(int)=0;
+
+        virtual void setEffect(std::string)=0;
+        virtual int getEffectBonus()=0;
+        virtual void setEffectBonus()=0;
+
+
+       
+
+
+        
+    private:
+      
+};
+
+
+
+
+
+#endif 
